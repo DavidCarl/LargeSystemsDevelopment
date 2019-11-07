319ba7a1ea19094e23d29cc4a2045cffb93914db
David <david.martin.carl@gmail.com>
2019-11-07 08:55:16 +0100

    Added ejb dependency

--- src/main/java/contract/interfaces/BeanInterface.java
+++ src/main/java/contract/interfaces/BeanInterface.java
@@ -2,9 +2,11 @@ package contract.interfaces;
 
 import contract.dto.*;
 
+import javax.ejb.Remote;
 import java.util.Collection;
 import java.util.Date;
 
+@Remote
 public interface BeanInterface {
     Collection<FlightRoute> getFlightRoutes(Date start, Date end, String depIata, String destIata, boolean oneWay);
     Booking makeBooking(Collection<Long> flightIds, FFNCCIdenitfier ffncc, Collection<Passenger> passengers);
